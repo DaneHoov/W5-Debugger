2d7296f8f78f0b4485298184d56188773b4df8e0
danehoov <DaneHoov22@gmail.com>
2024-04-09 22:54:00 -0600

    01 solved

--- problems/01-should-recycle.js
+++ problems/01-should-recycle.js
@@ -14,9 +14,10 @@ return `Recycle Me!`
 */
 
 function shouldRecycle(item) {
-  if (!item.plastic) {
+
+  if (item.plastic && item.color !== 'black') {
     return 'Recycle Me!'
-  } else if (item.color === 'black') {
+  } else if (item.plastic && item.color === 'black') {
     return 'Currently, cannot be recycled.'
   } else if (item.aluminum) {
     return 'Recycle Me!'
@@ -61,4 +62,5 @@ const styrofoamContainer = {
   paper: false
 };
 
+debugger
 console.log(shouldRecycle(styrofoamContainer)); // 'Cannot be recycled.'
